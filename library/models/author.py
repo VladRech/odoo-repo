83b9003b6bb4bf99230e2f4c835ee202bed9871a
Vlad Rechleckyj <rechvlad2022@gmail>
2023-01-01 20:53:16 +0200

    new_library 15

---
+++ library/models/author.py
@@ -0,0 +1,10 @@
+import logging
+from odoo import models, fields
+_logger = logging.getLogger(__name__)
+
+
+class Author(models.Model):
+    _name = 'lib.author'
+    _description = 'Author'
+    name = fields.Char()
+    active = fields.Boolean(default=True, )
