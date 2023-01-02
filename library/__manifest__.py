ab1d832ff308e018be8c71931fe27cbe8487d522
Vlad Rechleckyj <rechvlad2022@gmail>
2023-01-02 19:08:15 +0200

    library 3

---
+++ library/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "Library",
+    "summary": "Learning Odoo Community Add-Ons",
+    "author": "Vladyslav Rechleckyj",
+    'website': "https://letu.lviv.ua",
+    "category": "Education",
+    "version": "15.0.1.0.0",
+    "license": "OPL-1",
+    "depends": [],
+    'external_dependencies': {'python': [], },
+    "qweb": [],
+    "data": [
+        "security/ir.model.access.csv",
+        'views/menu.xml',
+        'views/book.xml',
+        'views/author.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    # 'images': [
+    #            'static/description/books.png',
+    #            'static/description/icon.png',
+    #            ],
+    'price': 0,
+    'currency': 'EUR',
+}
