bd7bad512f2b7d46369e25b0ed9709f3c6dd20ee
Vlad Rechleckyj <rechvlad2022@gmail.com>
2023-06-14 14:48:01 +0300

    Library 23

---
+++ olibrary/models/book.py
@@ -0,0 +1,14 @@
+import logging
+from odoo import models, fields
+_logger = logging.getLogger(__name__)
+
+
+class Book(models.Model):
+    _name = 'lib.book'
+    _description = 'Book'
+    _order = 'name'
+    name = fields.Char()
+    active = fields.Boolean(default=True,)
+    isbn = fields.Char()
+    author_ids = fields.Many2many(
+        comodel_name='lib.author', )
